0747b61504140b1bc1bff48207d6615ed9c537a5
鶴見侑樹 <tsurumi0302@gmail.com>
2018-10-27 00:01:28 +0900

    修正

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final WordPair = new WordPair.random()
+    final wordPair = new WordPair.random();
     return MaterialApp(
       title: 'welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text('Welcome to Flutter'),
-      ),
+        ),
         body: new Center(
           child: new Text(wordPair.asPascalCase),
         ),
-      )
-    )
+      ),
+    );
   }
 }
